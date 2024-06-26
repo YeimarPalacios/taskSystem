eea7748738f9b7d05d21763d60c3851219c11f97
Tatiana Hoyos Vertel <tatiana.hoyos@globalmvm.com>
2024-06-26 09:25:25 -0500

    se resuelven conflictos con el logout

--- api_authorization/urls.py
+++ api_authorization/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('api/authentication/', LoginView.as_view(), name='authentication'),
     path('api/introspect/', IntrospectView.as_view(), name='introspect'),
     path('api/refresh_token/', RefreshTokenView.as_view(), name='refresh_token'),
-    path('api/logout/', LogoutView.as_view(), name='logout'),
+    path('api/logout/', LogoutView.as_view(), name='api_logout'),
 ]
